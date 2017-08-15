c11c602bd0628cbf320b17123858c2dfa3700fff
Gabriel Novotny <gabriel.novotny@immosky.ch>
2017-08-15 13:15:16 +0200

    fix

--- js/index.js
+++ js/index.js
@@ -7,17 +7,17 @@
  */
 
 // Bootstrap components
-import "bootstrap/js/src/alert";
-import "bootstrap/js/src/button";
-import "bootstrap/js/src/carousel";
-import "bootstrap/js/src/collapse";
-import "bootstrap/js/src/dropdown";
-import "bootstrap/js/src/modal";
-import "bootstrap/js/src/popover";
-import "bootstrap/js/src/scrollspy";
-import "bootstrap/js/src/tab";
-import "bootstrap/js/src/tooltip";
-import "bootstrap/js/src/util";
+import "~bootstrap/js/src/alert";
+import "~bootstrap/js/src/button";
+import "~bootstrap/js/src/carousel";
+import "~bootstrap/js/src/collapse";
+import "~bootstrap/js/src/dropdown";
+import "~bootstrap/js/src/modal";
+import "~bootstrap/js/src/popover";
+import "~bootstrap/js/src/scrollspy";
+import "~bootstrap/js/src/tab";
+import "~bootstrap/js/src/tooltip";
+import "~bootstrap/js/src/util";
 
 // invalidComponentMatches is currently disabled due to https://github.com/rollup/rollup/issues/428#issuecomment-170066452
 import "./checkbox";
